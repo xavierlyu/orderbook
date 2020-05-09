9403d3a8555a7018849c4972889c18d378fae56e
Xavier Lu <xll2@cornell.edu>
2020-05-08 23:29:15 -0400

    added time-insensitive features

--- fetch_data.py
+++ fetch_data.py
@@ -20,24 +20,47 @@ database = config["database"]["database"]
 con = pymysql.connect(host, user=user, port=port, passwd=password, database=database)
 
 with con:
-    query = "SELECT * FROM `kucoin`.`ethusdt` ORDER BY `record_time` ASC LIMIT 7000"  # HAS TO BE ASCENDING ORDER
+    query = "SELECT * FROM `kucoin`.`ethusdt` ORDER BY `record_time` ASC LIMIT 25"  # HAS TO BE ASCENDING ORDER
     df = pd.read_sql(query, con)
 
-    # calculating spread and midprice
-    df["spread"] = df["ask1_price"] - df["bid1_price"]
-    df["midprice"] = (df["ask1_price"] + df["bid1_price"]) / 2.0
+    # bid-ask spreads, mid-prices, and price differences
+    for l in range(1, 11):
+        df[f"spread_{l}"] = df[f"ask{l}_price"] - df[f"bid{l}_price"]
+        df[f"midprice_{l}"] = (df[f"ask{l}_price"] + df[f"bid{l}_price"]) / 2.0
 
-    # calculating momentum
-    df["momentum-15"] = df["midprice"] - df["midprice"].shift(15)
+        if l > 1:
+            df[f"ask_diff_{l}"] = df[f"ask{l}_price"] - df["ask1_price"]
+            df[f"bid_diff_{l}"] = df["bid1_price"] - df[f"bid{l}_price"]
+
+    # mean prices and volumes
+    vols = [name for name in list(df.columns) if "_vol" in name]
+    prices = [name for name in list(df.columns) if "_price" in name]
+    ask_prices = [name for name in prices if "ask" in name]
+    bid_prices = [name for name in prices if "bid" in name]
+    ask_vols = [name for name in vols if "ask" in name]
+    bid_vols = [name for name in vols if "bid" in name]
+
+    df["avg_ask_price"] = df[ask_prices].mean(axis=1)
+    df["avg_bid_price"] = df[bid_prices].mean(axis=1)
+    df["avg_ask_vol"] = df[ask_vols].mean(axis=1)
+    df["avg_bid_vol"] = df[bid_vols].mean(axis=1)
+
+    # accumulated differences
+    df["acc_price_diff"] = 0
+    df["acc_vol_diff"] = 0
+    for l in range(1, 11):
+        df["acc_price_diff"] = (
+            df["acc_price_diff"] + df[f"ask{l}_price"] - df[f"bid{l}_price"]
+        )
+        df["acc_vol_diff"] = df["acc_vol_diff"] + df[f"ask{l}_vol"] - df[f"bid{l}_vol"]
 
     # calculating OBV
-    vols = [name for name in list(df.columns) if "_vol" in name]
     df["vol"] = df[vols].sum(axis=1)
     df["OBV"] = -1614347.8061
     for i in range(1, len(df)):
-        if df.loc[i, "midprice"] > df.loc[i - 1, "midprice"]:
+        if df.loc[i, "midprice_1"] > df.loc[i - 1, "midprice_1"]:
             df.loc[i, "OBV"] = df.loc[i - 1, "OBV"] + df.loc[i, "vol"]
-        elif df.loc[i, "midprice"] < df.loc[i - 1, "midprice"]:
+        elif df.loc[i, "midprice_1"] < df.loc[i - 1, "midprice_1"]:
             df.loc[i, "OBV"] = df.loc[i - 1, "OBV"] - df.loc[i, "vol"]
         else:
             df.loc[i, "OBV"] = df.loc[i - 1, "OBV"]
@@ -46,29 +69,29 @@ with con:
     df["time_diff"] = df["record_time"].shift(-5) - df["record_time"]
     df["time_diff"] = df["time_diff"] / np.timedelta64(1, "s")
     df["movement"] = (
-        (df["midprice"].shift(-5) - df["midprice"]) > (df["midprice"] * FEE)
+        (df["midprice_1"].shift(-5) - df["midprice_1"]) > (df["midprice_1"] * FEE)
     ) * 1  # converting a serie of bool to a serie of int
 
-    df = df.drop(columns=["spread", "record_time", "time_diff", "vol"])
+    df = df.drop(columns=["record_time", "time_diff", "vol", "OBV"])
 
     df = df.dropna()
     output_arr = []
 
-    # pd.set_option("display.max_rows", None)
-    # print(df)
+    pd.set_option("display.max_rows", None)
+    print(df)
 
-    for i in range(len(df)):
-        pair = {
-            "price": df.iloc[i].values[:-1].tolist(),
-            "class": str(df.iloc[i, -1]),
-        }
-        output_arr.append(pair)
+    # for i in range(len(df)):
+    #     pair = {
+    #         "price": df.iloc[i].values[:-1].tolist(),
+    #         "class": str(df.iloc[i, -1]),
+    #     }
+    #     output_arr.append(pair)
 
     # print(output_arr)
 
-    output_file_path = "./train.json"
-    with open(output_file_path, "w") as f:
-        json.dump(output_arr, f)
+    # output_file_path = "./train.json"
+    # with open(output_file_path, "w") as f:
+    #     json.dump(output_arr, f)
 
     # fig, ax1 = plt.subplots()
     # color = "tab:red"
